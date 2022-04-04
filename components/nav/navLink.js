c2594b14036d472faa7ba030781f7cc99e92f18a
Christian Predoianu <christianpredoianu@yahoo.com>
2022-04-04 17:39:50 +0200

    destructured props

--- components/nav/navLink.js
+++ components/nav/navLink.js
@@ -1,19 +1,19 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
 
-export default function NavLink(props) {
+export default function NavLink({ path, closeNav, title }) {
   const router = useRouter();
   const currentRoute = router.pathname;
 
   return (
     <li
       className={`block mt-4 md:inline-block md:mt-0 mr-4 dark:text-gray-200 hover:text-blue-600 ${
-        currentRoute === props.path ? "text-blue-700 dark:text-gray-400" : ""
+        currentRoute === path ? "text-blue-700 dark:text-gray-400" : ""
       }`}
-      onClick={props.closeNav}
+      onClick={closeNav}
     >
-      <Link href={props.path}>
-        <a>{props.title}</a>
+      <Link href={path}>
+        <a>{title}</a>
       </Link>
     </li>
   );
