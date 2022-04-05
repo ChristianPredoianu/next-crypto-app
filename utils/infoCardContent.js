ed1243d627df954534bf2e211d6b0dbe1880d575
Christian Predoianu <christianpredoianu@yahoo.com>
2022-04-05 15:07:54 +0200

    home info card data

---
+++ utils/infoCardContent.js
@@ -0,0 +1,17 @@
+export const infoCardData = [
+  {
+    title: "Payoff",
+    content:
+      "Interactively procrastinate high-payoff content without backward-compatible data. Quickly cultivate optimal processes and tactical architectures.",
+  },
+  {
+    title: "Testing",
+    content:
+      "Quickly aggregate B2B users and worldwide potentialities. Progressively plagiarize resource-leveling e-commerce through resource-leveling core competencies.",
+  },
+  {
+    title: "Vision",
+    content:
+      "Quickly aggregate B2B users and worldwide potentialities. Progressively plagiarize resource-leveling e-commerce through resource-leveling core competencies.",
+  },
+];
