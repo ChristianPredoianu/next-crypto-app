d1249100bdf33189d55999afb71ab1666b245e9a
Christian Predoianu <christianpredoianu@yahoo.com>
2022-04-06 14:31:02 +0200

    changed margin

--- components/nav/DarkModeToggler.js
+++ components/nav/DarkModeToggler.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import useDarkMode from "@/hooks/useDarkMode";
+import { useState } from 'react';
+import useDarkMode from '@/hooks/useDarkMode';
 
 export default function DarkModeToggler() {
   let [isDarkMode, setIsDarkMode] = useState(false);
@@ -13,16 +13,16 @@ export default function DarkModeToggler() {
 
   return (
     <div className="flex flex-1 items-center ml-4">
-      <p className="text-xs mr-2 dark:text-gray-200">Dark Mode</p>
+      <p className="text-xs mr-2 dark:text-gray-300">Dark Mode</p>
       <div
         className={`w-12 h-6 items-center rounded-full p-1 duration-300 ease-in-out cursor-pointer ${
-          isDarkMode ? "bg-green-500" : "bg-black"
+          isDarkMode ? 'bg-green-500' : 'bg-black'
         }`}
         onClick={toggleDarkModeHandler}
       >
         <div
           className={`w-4 h-4 rounded-full shadow-md bg-white transform duration-300 ease-in-out ${
-            isDarkMode ? "translate-x-6" : "translate-x-0"
+            isDarkMode ? 'translate-x-6' : 'translate-x-0'
           }`}
         ></div>
       </div>
