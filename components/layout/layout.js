166ea8d75d626a24871b41774aff3c183e210ced
Christian Predoianu <christianpredoianu@yahoo.com>
2022-04-06 14:25:27 +0200

    changed dark mode color

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -2,7 +2,7 @@ import Nav from "@/components/nav/Nav";
 
 export default function Layout({ children }) {
   return (
-    <div className="min-h-screen dark:bg-slate-800 dark:text-gray-200">
+    <div className="min-h-screen dark:bg-clr-dark-theme dark:text-gray-300">
       <Nav />
       <div className="container mx-auto">{children}</div>
     </div>
