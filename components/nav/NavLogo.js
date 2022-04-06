51cb132d4a7032c5e538668a9508607cf9d96c7f
Christian Predoianu <christianpredoianu@yahoo.com>
2022-04-06 14:31:27 +0200

    changed margin

--- components/nav/NavLogo.js
+++ components/nav/NavLogo.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function NavLogo() {
   return (
     <Link href="/">
-      <a className="text-black font-bold dark:text-gray-200 md:mr-4">
+      <a className="text-black font-bold dark:text-gray-300 md:mr-4">
         CryptoInfo.
       </a>
     </Link>
