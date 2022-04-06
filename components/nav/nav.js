3419b7c1dc779f9b78606256c259dbba633bb5ae
Christian Predoianu <christianpredoianu@yahoo.com>
2022-04-06 14:31:41 +0200

    changed margin

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -1,15 +1,15 @@
 import NavLogo from "@/components/nav/NavLogo";
-import DarkModeToggler from "./DarkModeToggler";
-import HamburgerMenu from "./HamburgerMenu";
+import DarkModeToggler from "@/components/nav/DarkModeToggler";
+import Menu from "@/components/nav/Menu";
 
 export default function Nav() {
   return (
     <>
-      <header className="w-full container mx-auto py-6 sm:px-4">
-        <nav className="w-full flex justify-between items-center flex-wrap p-2">
+      <header className="w-full container mx-auto py-6">
+        <nav className="w-full flex justify-between items-center flex-wrap py-2">
           <NavLogo />
           <DarkModeToggler />
-          <HamburgerMenu />
+          <Menu />
         </nav>
       </header>
       <div className="border-b border-b-black dark:border-b-gray-200"></div>
