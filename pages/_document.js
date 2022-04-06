8512e373b6628af50fb58a980ba07f531b7e6bd4
Christian Predoianu <christianpredoianu@yahoo.com>
2022-04-06 14:27:03 +0200

    added font 200

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   render() {
@@ -6,7 +6,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;700&display=swap"
             rel="stylesheet"
           ></link>
         </Head>
