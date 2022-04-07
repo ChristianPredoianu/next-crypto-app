b075042e647c16dd0e81c55504b44c194fc27c33
Christian Predoianu <christianpredoianu@yahoo.com>
2022-04-07 11:33:12 +0200

    work on content from api + darkmode + switch colors for different currency

--- components/cards/CryptoCard.js
+++ components/cards/CryptoCard.js
@@ -1,17 +1,54 @@
 import Image from 'next/image';
-import BitcoinImg from '@/assets/images/bitcoin.png';
 
-export default function CryptoCard() {
+export default function CryptoCard({
+  currencyName,
+  currencySymbol,
+  currencyRank,
+  currencyImg,
+  currencyPrice,
+  currencyMarketCap,
+  currencyVolume,
+  currencySupply,
+}) {
+  const currencyFormatter = new Intl.NumberFormat('en-us', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 6,
+  });
+
+  const cryptoCurrency = currencyName;
+  let cardGradient;
+
+  switch (cryptoCurrency) {
+    case 'Bitcoin':
+      cardGradient = 'from-orange-400';
+      break;
+    case 'Ethereum':
+      cardGradient = 'from-purple-400';
+      break;
+    case 'Tether':
+      cardGradient = 'from-green-400';
+      break;
+    case 'BNB':
+      cardGradient = 'from-yellow-400';
+      break;
+    case 'USD Coin':
+      cardGradient = 'from-blue-400';
+  }
+
   return (
     <>
       <div className="flex flex-col items-center sm:flex sm:flex-row ">
-        <div className="relative py-20 px-20 bg-gradient-to-b from-orange-500 to-gray-200 shadow-lg shadow-gray-400 sm:py-32">
-          <p className="absolute bottom-6 -right-4 rotate-90 text-4xl text-gray-600 opacity-60 tracking-widest ">
-            BTC
+        <div
+          className={`relative py-20 px-20 bg-gradient-to-b ${cardGradient} to-gray-200 shadow-lg shadow-gray-400 dark:shadow-sm sm:py-32`}
+        >
+          <p className="absolute bottom-2 right-2 text-4xl text-gray-600 opacity-60  ">
+            {currencySymbol.toUpperCase()}
           </p>
           <div className="absolute top-2 left-2 w-3/6 ">
             <Image
-              src={BitcoinImg}
+              src={currencyImg}
               alt="crypto"
               layout="responsive"
               width={107}
@@ -20,26 +57,37 @@ export default function CryptoCard() {
             />
           </div>
         </div>
-        <div className="font-extralight mt-8 text-gray-900 px-10 shadow-lg shadow-gray-400 border border-gray-200 sm:mt-0">
+        <div className="w-11/12 px-10 py-4 mt-6 font-extralight text-gray-900 dark:text-gray-300 shadow-lg shadow-gray-400 dark:shadow-sm dark:shadow-gray-600 sm:mt-0">
           <div className="py-2">
-            <h2 className="text-2xl uppercase tracking-wider">Bitcoin</h2>
-            <p className="text-md text-orange-400">BTC</p>
+            <h2 className="text-2xl uppercase tracking-wider">
+              {currencyName}
+            </h2>
+            <p className="text-lg text-orange-400">{currencySymbol}</p>
           </div>
           <div className="flex py-2">
-            <h3 className="text-4xl">$9521512</h3>
-            <p className="text-md text-green-400 ml-2">1.6%</p>
+            <h3 className="text-4xl">{`${currencyFormatter.format(
+              currencyPrice
+            )}`}</h3>
+            <p className="text-xl text-green-600 ml-2">{`Rank: ${currencyRank}`}</p>
           </div>
           <div className="py-2">
-            <p className="text-2xl">1561561651</p>
-            <p className="text-md text-gray-500">Market Cap</p>
+            <p className="text-2xl">{`${currencyFormatter.format(
+              currencyMarketCap
+            )}`}</p>
+            <p className="text-lg text-gray-500">Market Cap</p>
           </div>
           <div className="py-2">
-            <p className="text-2xl">1561561651</p>
-            <p className="text-xs text-gray-500">Volume</p>
+            <p className="text-2xl">{`${currencyFormatter.format(
+              currencyVolume
+            )}`}</p>
+            <p className="text-lg text-gray-500">Volume</p>
           </div>
           <div className="py-2">
-            <p className="text-2xl">1561561651</p>
-            <p className="text-xs text-gray-500">Circulating Supply</p>
+            <p className="text-2xl">{`$ ${currencySupply.toLocaleString(
+              'en-us',
+              { minimumFractionDigits: 2 }
+            )}`}</p>
+            <p className="text-lg text-gray-500">Circulating Supply</p>
           </div>
         </div>
       </div>
