8560ac17b85f09e0b012eafd91a5a0d1f348c13a
Christian Predoianu <christianpredoianu@yahoo.com>
2022-04-11 15:46:36 +0200

    added useScrollToSection

--- pages/exchange/index.js
+++ pages/exchange/index.js
@@ -1,14 +1,34 @@
+import { useRef } from 'react';
+import useScrollToSection from '@/hooks/useScrollToSection';
 import ExchangeHeroSection from '@/components/hero-sections/ExchangeHeroSection';
 import CryptoCard from '@/components/cards/CryptoCard';
 import CryptoList from '@/components/crypto-list/CryptoList';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
 
 export default function ExchangePage({ currencyData }) {
   const topFiveCurrencies = currencyData.slice(0, 5);
 
+  const cardsSectionRef = useRef(null);
+  const listRef = useRef(null);
+
+  const [scrollToSection] = useScrollToSection();
+
+  function scrollToCardsSectionHandler() {
+    scrollToSection(cardsSectionRef);
+  }
+
+  function scrollToListHandler() {
+    scrollToSection(listRef);
+  }
+
   return (
     <>
-      <ExchangeHeroSection />
-      <h2 className="text-2xl text-center md:text-4xl lg:text-5xl pt-20 md:pt-40 lg:pt-64">
+      <ExchangeHeroSection onScrollToCards={scrollToCardsSectionHandler} />
+      <h2
+        className="text-2xl text-center md:text-4xl lg:text-5xl pt-20 md:pt-40 lg:pt-64"
+        ref={cardsSectionRef}
+      >
         Top 5 cryptocurrencies
       </h2>
       <section className="flex flex-wrap justify-center gap-20 md:gap-20 my-20 md:my-32 lg:my-52">
@@ -26,9 +46,12 @@ export default function ExchangePage({ currencyData }) {
           />
         ))}
       </section>
-      <section className="py-20">
+      <section className="py-20" ref={listRef}>
         <CryptoList currencyData={currencyData} />
       </section>
+      <div className="text-4xl text-center animate-bounce cursor-pointer">
+        <FontAwesomeIcon icon={faArrowUp} onClick={scrollToListHandler} />
+      </div>
     </>
   );
 }
