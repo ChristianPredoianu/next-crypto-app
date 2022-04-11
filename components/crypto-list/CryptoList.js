33129519c72a025e52805515310ed37b0ff6e915
Christian Predoianu <christianpredoianu@yahoo.com>
2022-04-11 15:41:49 +0200

    added sort functionality + active arrow

--- components/crypto-list/CryptoList.js
+++ components/crypto-list/CryptoList.js
@@ -3,13 +3,13 @@ import CryptoListMenu from '@/components/crypto-list/CryptoListMenu';
 import CryptoListItem from '@/components/crypto-list/CryptoListItem';
 
 export default function CryptoList({ currencyData }) {
-  let [dataToMap, setDataToMap] = useState(currencyData);
-  let [sortType, setSortType] = useState(null);
+  const [dataToMap, setDataToMap] = useState(currencyData);
+  const [activeArrow, setActiveArrow] = useState(null);
 
-  function sortDataHandler(dataToSort, order = null) {
-    setSortType(order);
+  function sortDataHandler(dataToSort, arrowId, order = null) {
+    setActiveArrow(arrowId);
 
-    const sortedData = currencyData.sort((a, b) =>
+    const sortedData = [...currencyData].sort((a, b) =>
       order === 'ascending'
         ? b[dataToSort] - a[dataToSort]
         : a[dataToSort] - b[dataToSort]
@@ -18,26 +18,33 @@ export default function CryptoList({ currencyData }) {
     setDataToMap(sortedData);
   }
 
+  function resetInitialDataHandler() {
+    setActiveArrow(null);
+    setDataToMap(currencyData);
+  }
+
   return (
     <>
-      <CryptoListMenu onSortData={sortDataHandler} />
-      <div className="flex w-11/12 items-center mx-auto bg-red-500"></div>
-      <div className="">
-        <ul className="flex flex-col gap-4">
-          {dataToMap.map((currency) => (
-            <CryptoListItem
-              key={currency.id}
-              currencyName={currency.name}
-              currencySymbol={currency.symbol}
-              currencyPrice={currency.current_price}
-              currencyImg={currency.image}
-              currencyChangePercent={currency.price_change_percentage_24h}
-              currencyVolume={currency.total_volume}
-              currencyMarketCap={currency.market_cap}
-            />
-          ))}
-        </ul>
-      </div>
+      <CryptoListMenu
+        activeArrow={activeArrow}
+        onResetInitialData={resetInitialDataHandler}
+        onSortData={sortDataHandler}
+      />
+
+      <ul className="flex flex-col gap-4">
+        {dataToMap.map((currency) => (
+          <CryptoListItem
+            key={currency.id}
+            currencyName={currency.name}
+            currencySymbol={currency.symbol}
+            currencyPrice={currency.current_price}
+            currencyImg={currency.image}
+            currencyChangePercent={currency.price_change_percentage_24h}
+            currencyVolume={currency.total_volume}
+            currencyMarketCap={currency.market_cap}
+          />
+        ))}
+      </ul>
     </>
   );
 }
