9fe725305db76a4f3a747d6065220f2deafaef1a
Christian Predoianu <christianpredoianu@yahoo.com>
2022-04-11 15:44:18 +0200

    added active arrow

--- components/crypto-list/CryptoListMenuItem.js
+++ components/crypto-list/CryptoListMenuItem.js
@@ -1,32 +1,39 @@
-import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretUp, faCaretDown } from '@fortawesome/free-solid-svg-icons';
 
 export default function CryptoListMenuItem({
   name,
   index,
+  arrowIdUp,
+  arrowIdDown,
+  activeArrow,
   onSortAscending,
   onSortDescending,
 }) {
   return (
     <div
-      className={`flex items-center bg-blue-500 ${
-        index !== 1 ? 'hidden' : ''
-      } ${index !== 0 ? 'md:block' : 'visible'}  ${
-        index === 0 ? ' md:block md:invisible' : ''
-      }`}
+      className={`w-full  ${index !== 0 && index !== 1 ? 'hidden' : ''} 
+       md:block`}
     >
       <p>{name}</p>
-      <div className="flex flex-col ml-2">
+      <div
+        className={`flex flex-col w-full items-start ml-2 ${
+          index === 0 ? 'hidden' : ''
+        }`}
+      >
         <FontAwesomeIcon
           icon={faCaretUp}
-          className="cursor-pointer"
-          onClick={onSortAscending}
+          className={`${
+            arrowIdUp === activeArrow ? 'text-green-500' : ''
+          } cursor-pointer `}
+          onClick={() => onSortAscending(arrowIdUp)}
         />
         <FontAwesomeIcon
           icon={faCaretDown}
-          className="cursor-pointer"
-          onClick={onSortDescending}
+          className={`${
+            arrowIdDown === activeArrow ? 'text-green-500' : ''
+          } cursor-pointer`}
+          onClick={() => onSortDescending(arrowIdDown)}
         />
       </div>
     </div>
