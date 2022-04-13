770dc2c6f13bf3544b0a4111cc6b85e70246267b
Christian Predoianu <christianpredoianu@yahoo.com>
2022-04-13 15:12:29 +0200

    fixed a bug

--- hooks/useCurrencyGradient.js
+++ hooks/useCurrencyGradient.js
@@ -1,24 +1,26 @@
-export default function useCurrencyGradient(currencyName) {
-  const cryptoCurrency = currencyName;
+export default function useCurrencyGradient() {
   let cardGradient;
 
-  switch (cryptoCurrency) {
-    case 'Bitcoin':
-      cardGradient = 'from-orange-400';
-      break;
-    case 'Ethereum':
-      cardGradient = 'from-purple-400';
-      break;
-    case 'Tether':
-      cardGradient = 'from-green-400';
-      break;
-    case 'BNB':
-      cardGradient = 'from-yellow-400';
-      break;
-    case 'USD Coin':
-      cardGradient = 'from-blue-400';
-      break;
-  }
+  function switchGradients(name) {
+    switch (name) {
+      case 'Bitcoin':
+        cardGradient = 'from-orange-400';
+        break;
+      case 'Ethereum':
+        cardGradient = 'from-purple-400';
+        break;
+      case 'Tether':
+        cardGradient = 'from-green-400';
+        break;
+      case 'BNB':
+        cardGradient = 'from-yellow-400';
+        break;
+      case 'USD Coin':
+        cardGradient = 'from-blue-400';
+        break;
+    }
 
-  return [cardGradient];
+    return cardGradient;
+  }
+  return [switchGradients];
 }
