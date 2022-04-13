a29ad9f7f22e008a1dc45a57ad8f611d314587c4
Christian Predoianu <christianpredoianu@yahoo.com>
2022-04-13 15:13:22 +0200

    final

--- components/cards/CryptoInfoCard.js
+++ components/cards/CryptoInfoCard.js
@@ -1,15 +1,18 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import useCurrencyGradient from '@/hooks/useCurrencyGradient';
 import useCurrencyFormatter from '@/hooks/useCurrencyFormatter';
 
-export default function CryptoInfoCard({ name, symbol, img, price }) {
-  const [cardGradient] = useCurrencyGradient(name);
+export default function CryptoInfoCard({ name, id, symbol, img, price }) {
+  const [switchGradients] = useCurrencyGradient();
   const [currencyFormatter] = useCurrencyFormatter();
 
   return (
     <div className="shadow-lg shadow-gray-500 dark:shadow-slate-800 w-2/3 sm:w-2/4 md:w-2/5 lg:w-min rounded-xl">
       <div
-        className={`relative py-4 px-4 bg-gradient-to-b ${cardGradient} rounded-t-xl border-b border-gray-500`}
+        className={`relative py-4 px-4 bg-gradient-to-b ${switchGradients(
+          name
+        )} rounded-t-xl border-b border-gray-500`}
       >
         <Image
           src={img}
@@ -35,16 +38,19 @@ export default function CryptoInfoCard({ name, symbol, img, price }) {
       </div>
       <div className="relative py-20 bg-gray-200 dark:bg-black dark:border-b dark:border-gray-500 md:px-28">
         <h4 className="absolute w-2/4 bottom-4 -right-3 bg-green-500 rounded-sm p-2 text-right">
-          {/* DONT FORGET TO FORMAT CURRENCIES */}{' '}
           {`${currencyFormatter.format(price)}`}
         </h4>
       </div>
       <div className="py-4 text-center text-gray-600 dark:text-gray-200">
-        <button
-          className={`bg-gradient-to-r ${cardGradient} text-center py-2 px-4 border border-gray-500`}
-        >
-          More Info
-        </button>
+        <Link href={`/currencyinfo/${id}`}>
+          <button
+            className={`bg-gradient-to-r ${switchGradients(
+              name
+            )} text-center py-2 px-4 border border-gray-500`}
+          >
+            More Info
+          </button>
+        </Link>
       </div>
     </div>
   );
