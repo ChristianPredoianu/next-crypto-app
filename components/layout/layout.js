3db88efb12febccf51934c093042f8a3a0cf3883
Christian Predoianu <christianpredoianu@yahoo.com>
2022-04-13 15:12:49 +0200

    removed div container

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,10 +1,12 @@
-import Nav from "@/components/nav/Nav";
+import Nav from '@/components/nav/Nav';
 
 export default function Layout({ children }) {
   return (
-    <div className="min-h-screen dark:bg-clr-dark-theme dark:text-gray-300">
+    <>
       <Nav />
-      <div className="container mx-auto">{children}</div>
-    </div>
+      <main className="dark:bg-clr-dark-theme dark:text-gray-300">
+        {children}
+      </main>
+    </>
   );
 }
