813a8e49bd26c9b9abcb76665c1fbb67a0c8a3a5
Christian Predoianu <christianpredoianu@yahoo.com>
2022-04-19 11:02:17 +0200

    destructured currencyData

--- components/cards/about-crypto-card/AboutCryptoCard.js
+++ components/cards/about-crypto-card/AboutCryptoCard.js
@@ -1,39 +1,33 @@
-import { useRef } from 'react';
 import Image from 'next/image';
 import useCurrencyFormatter from '@/hooks/useCurrencyFormatter';
 import AboutCryptoCardList from '@/components/cards/about-crypto-card/AboutCryptoCardList';
 
-export default function AboutCryptoCard({
-  name,
-  symbol,
-  rank,
-  image,
-  marketData,
-}) {
+export default function AboutCryptoCard({ currencyData, cardRef }) {
+  const { name, symbol, rank, image, market_data } = currencyData;
+
   const [currencyFormatter] = useCurrencyFormatter();
-  const cardRef = useRef(null);
 
   const market = [
     { title: 'Rank:', data: rank },
     {
       title: 'Price:',
-      data: currencyFormatter.format(marketData.current_price.usd),
+      data: currencyFormatter.format(market_data.current_price.usd),
     },
     {
       title: 'Price Change:',
-      data: `${marketData.price_change_percentage_24h} %`,
+      data: `${market_data.price_change_percentage_24h} %`,
     },
     {
       title: 'Supply:',
-      data: currencyFormatter.format(marketData.circulating_supply),
+      data: currencyFormatter.format(market_data.circulating_supply),
     },
     {
       title: 'Market Cap:',
-      data: currencyFormatter.format(marketData.market_cap.usd),
+      data: currencyFormatter.format(market_data.market_cap.usd),
     },
     {
       title: 'Volume:',
-      data: currencyFormatter.format(marketData.total_volume.usd),
+      data: currencyFormatter.format(market_data.total_volume.usd),
     },
   ];
   return (
@@ -43,7 +37,13 @@ export default function AboutCryptoCard({
     >
       <div className="flex justify-around items-center mt-8">
         <h2 className="text-4xl">{name}</h2>
-        <Image src={image} alt="crypto" layout="fixed" width={80} height={80} />
+        <Image
+          src={image.large}
+          alt="crypto"
+          layout="fixed"
+          width={80}
+          height={80}
+        />
       </div>
       <h4 className="text-center mt-2">{`(${symbol})`}</h4>
       <div className="flex justify-center py-4">
