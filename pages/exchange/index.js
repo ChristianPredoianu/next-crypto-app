501536876f089558a5c585d755c4f413a3dfa50f
Christian Predoianu <christianpredoianu@yahoo.com>
2022-04-19 11:04:57 +0200

    added gsap scrolltrigger animation

--- pages/exchange/index.js
+++ pages/exchange/index.js
@@ -3,23 +3,48 @@ import ExchangeHeroSection from '@/components/hero-sections/ExchangeHeroSection'
 import CryptoCard from '@/components/cards/CryptoCard';
 import CryptoList from '@/components/crypto-list/CryptoList';
 import ArrowUp from '@/components/Ui/ArrowUp';
+import useIsomorphicLayoutEffect from '@/hooks/useIsomorphicLayoutEffect';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 
 export default function ExchangePage({ currencyData }) {
   const topFiveCurrencies = currencyData.slice(0, 5);
 
+  gsap.registerPlugin(ScrollTrigger);
+  const tl = gsap.timeline({ duration: 1 });
+
   const cardsSectionRef = useRef(null);
+  const cardRef = useRef(null);
   const listRef = useRef(null);
+
+  let el = useRef();
+  let q = gsap.utils.selector(cardsSectionRef);
+
+  useIsomorphicLayoutEffect(() => {
+    tl.from(q('.card'), {
+      x: 100,
+      opacity: 0,
+      stagger: 0.25,
+      scrollTrigger: {
+        trigger: cardsSectionRef.current,
+        scrub: 1,
+        start: 'top bottom',
+        end: '20%',
+        markers: true,
+      },
+    });
+  }, []);
 
   return (
     <>
       <ExchangeHeroSection listRef={cardsSectionRef} />
-      <h2
-        className="text-2xl text-center md:text-4xl lg:text-5xl pt-20 md:pt-40 lg:pt-64"
-        ref={cardsSectionRef}
-      >
+      <h2 className="text-2xl text-center md:text-4xl lg:text-5xl pt-20 md:pt-40 lg:pt-64">
         Top 5 cryptocurrencies
       </h2>
-      <section className="flex flex-wrap justify-center gap-20 md:gap-20 my-20 md:my-32 lg:my-52">
+      <section
+        className="flex flex-wrap justify-center gap-20 md:gap-20 my-20 md:my-32 lg:my-52"
+        ref={cardsSectionRef}
+      >
         {topFiveCurrencies.map((currency) => (
           <CryptoCard
             key={currency.id}
@@ -31,6 +56,7 @@ export default function ExchangePage({ currencyData }) {
             currencyMarketCap={currency.market_cap}
             currencyVolume={currency.total_volume}
             currencySupply={currency.circulating_supply}
+            cardRef={cardRef}
           />
         ))}
       </section>
