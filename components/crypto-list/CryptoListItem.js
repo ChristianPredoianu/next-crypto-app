98eadaa31f1afeb3e67dfc0bf8c7a70680d71d54
Christian Predoianu <christianpredoianu@yahoo.com>
2022-04-20 12:02:48 +0200

    wrapped in memo props don't change

--- components/crypto-list/CryptoListItem.js
+++ components/crypto-list/CryptoListItem.js
@@ -1,22 +1,25 @@
+import { memo } from 'react';
 import Image from 'next/image';
 import useCurrencyFormatter from '@/hooks/useCurrencyFormatter';
 
-export default function CryptoListItem({
-  currencyName,
-  currencySymbol,
-  currencyPrice,
-  currencyImg,
-  currencyChangePercent,
-  currencyVolume,
-  currencyMarketCap,
-}) {
+export default memo(function CryptoListItem({ currency }) {
+  const {
+    name,
+    symbol,
+    current_price,
+    image,
+    price_change_percentage_24h,
+    total_volume,
+    market_cap,
+  } = currency;
+
   const [currencyFormatter] = useCurrencyFormatter();
 
   return (
     <li className="flex justify-between items-center w-11/12 mx-auto px-4 py-2 shadow-lg shadow-gray-300 dark:bg-slate-900 dark:shadow-sm dark:shadow-gray-800">
       <div className="w-full">
         <Image
-          src={currencyImg}
+          src={image}
           alt="crypto"
           layout="fixed"
           width={30}
@@ -25,33 +28,35 @@ export default function CryptoListItem({
         />
 
         <div className="w-full">
-          <p className="text-sm dark:text-purple-400">{currencyName}</p>
-          <p className="text-sm">{currencySymbol}</p>
+          <p className="text-sm dark:text-purple-400">{name}</p>
+          <p className="text-sm">{symbol}</p>
         </div>
       </div>
       <div className="w-full">
-        <p>{currencyFormatter(currencyPrice)}</p>
+        <p>{currencyFormatter(current_price)}</p>
         <p
           className={`md:hidden ${
-            currencyChangePercent > 0 ? 'text-green-500' : 'text-red-500'
+            price_change_percentage_24h > 0 ? 'text-green-500' : 'text-red-500'
           }`}
         >
-          {`${currencyChangePercent > 0 ? '+' : ''}${currencyChangePercent}`}
+          {`${
+            price_change_percentage_24h > 0 ? '+' : ''
+          }${price_change_percentage_24h}`}
         </p>
       </div>
       <p
         className={`hidden w-full md:block ${
-          currencyChangePercent > 0 ? 'text-green-500' : 'text-red-500'
+          price_change_percentage_24h > 0 ? 'text-green-500' : 'text-red-500'
         }`}
       >
-        {`${currencyChangePercent > 0 ? '+' : ''}${currencyChangePercent}`}
+        {`${
+          price_change_percentage_24h > 0 ? '+' : ''
+        }${price_change_percentage_24h}`}
       </p>
       <p className="hidden w-full md:block ">
-        {currencyFormatter(currencyVolume)}
+        {currencyFormatter(total_volume)}
       </p>
-      <p className="hidden w-full md:block">
-        {currencyFormatter(currencyMarketCap)}
-      </p>
+      <p className="hidden w-full md:block">{currencyFormatter(market_cap)}</p>
     </li>
   );
-}
+});
