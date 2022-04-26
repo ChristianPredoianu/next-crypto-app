a6db05d1b5ead0cf9e9c1b377594d04378dee16e
Christian Predoianu <christianpredoianu@yahoo.com>
2022-04-26 09:50:50 +0200

    added footer

---
+++ components/Footer.js
@@ -0,0 +1,16 @@
+export default function Footer() {
+  return (
+    <footer className="container mx-auto px-4 py-8 text-sm text-center">
+      <p>
+        Crypto market cap and pricing provided by{' '}
+        <a
+          href="https://www.coingecko.com/en/api/documentation"
+          target="blank"
+          rel="noopener noreferrer"
+        >
+          Coingecko.com
+        </a>{' '}
+      </p>
+    </footer>
+  );
+}
