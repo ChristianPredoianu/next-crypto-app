9d5ded8ca296ba122ca5655f8adf303891b0815c
Christian Predoianu <christianpredoianu@yahoo.com>
2022-04-26 10:52:08 +0200

    added classNames for conditional classnames

--- components/cards/about-crypto-card/AboutCryptoCardListItem.js
+++ components/cards/about-crypto-card/AboutCryptoCardListItem.js
@@ -1,13 +1,21 @@
+import classNames from 'classnames';
+
 export default function AboutCryptoCardListItem({ title, data, index }) {
   let color;
 
   data < 0 ? (color = 'text-red-500') : (color = 'text-green-500');
-
+  console.log(data);
   return (
     <li className="border-b border-b-gray-500">
       <div className="px-2">
         <h4 className="text-xl">{title}</h4>
-        <p className={`${index === 2 ? color : ''} py-2 text-lg`}>{data}</p>
+        <p
+          className={classNames('py-2', 'text-lg', {
+            [`${color}`]: index === 2,
+          })}
+        >
+          {data}
+        </p>
       </div>
     </li>
   );
