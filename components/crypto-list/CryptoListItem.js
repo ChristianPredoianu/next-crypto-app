4c7e22ed67f42ca3f1e41aa0bdf4e5d311b8274f
Christian Predoianu <christianpredoianu@yahoo.com>
2022-04-26 11:45:59 +0200

    added classNames for conditional classnames

--- components/crypto-list/CryptoListItem.js
+++ components/crypto-list/CryptoListItem.js
@@ -1,6 +1,7 @@
 import { memo } from 'react';
 import Image from 'next/image';
 import useCurrencyFormatter from '@/hooks/useCurrencyFormatter';
+import classNames from 'classnames';
 
 export default memo(function CryptoListItem({ currency }) {
   const {
@@ -15,17 +16,12 @@ export default memo(function CryptoListItem({ currency }) {
 
   const [currencyFormatter] = useCurrencyFormatter();
 
+  const isPriceChangePositive = price_change_percentage_24h > 0;
+
   return (
     <li className="flex justify-between items-center w-11/12 mx-auto px-4 py-2 shadow-lg shadow-gray-300 dark:bg-slate-900 dark:shadow-sm dark:shadow-gray-800">
       <div className="w-full">
-        <Image
-          src={image}
-          alt="crypto"
-          layout="fixed"
-          width={30}
-          height={30}
-          priority
-        />
+        <Image src={image} alt="crypto" layout="fixed" width={30} height={30} />
 
         <div className="w-full">
           <p className="text-sm dark:text-purple-400">{name}</p>
@@ -35,9 +31,10 @@ export default memo(function CryptoListItem({ currency }) {
       <div className="w-full">
         <p>{currencyFormatter(current_price)}</p>
         <p
-          className={`md:hidden ${
-            price_change_percentage_24h > 0 ? 'text-green-500' : 'text-red-500'
-          }`}
+          className={classNames('md:hidden', {
+            'text-green-500': isPriceChangePositive,
+            'text-red-500': !isPriceChangePositive,
+          })}
         >
           {`${
             price_change_percentage_24h > 0 ? '+' : ''
@@ -45,9 +42,10 @@ export default memo(function CryptoListItem({ currency }) {
         </p>
       </div>
       <p
-        className={`hidden w-full md:block ${
-          price_change_percentage_24h > 0 ? 'text-green-500' : 'text-red-500'
-        }`}
+        className={classNames('hidden', 'w-full', 'md:block', {
+          'text-green-500': isPriceChangePositive,
+          'text-red-500': !isPriceChangePositive,
+        })}
       >
         {`${
           price_change_percentage_24h > 0 ? '+' : ''
