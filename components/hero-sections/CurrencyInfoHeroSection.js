d6505e16d61654bf484ff1730f94763d5435982b
Christian Predoianu <christianpredoianu@yahoo.com>
2022-04-27 14:14:45 +0200

    moved info hero section to component

---
+++ components/hero-sections/CurrencyInfoHeroSection.js
@@ -0,0 +1,17 @@
+export default function CurrencyInfoHeroSection() {
+  return (
+    <section className="container mx-auto px-4 py-16 md:py-32">
+      <div className="md:w-2/4">
+        <h1 className="text-4xl">
+          Bitcoin and cryptocurrency for{' '}
+          <span className="text-purple-500">beginners</span>
+        </h1>
+        <h2 className="mt-4">
+          Get your information about the basics of cryprocurrencies from us. We
+          take a deep dive into different cryptocurrencies so that you can be
+          informed about them before you invest.
+        </h2>
+      </div>
+    </section>
+  );
+}
