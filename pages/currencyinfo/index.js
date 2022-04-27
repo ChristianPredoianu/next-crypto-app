d49162b140d09272c4c30c6cefc85f116468263a
Christian Predoianu <christianpredoianu@yahoo.com>
2022-04-27 14:14:59 +0200

    moved info hero section to component

--- pages/currencyinfo/index.js
+++ pages/currencyinfo/index.js
@@ -1,5 +1,6 @@
 import { useRef } from 'react';
 import Head from 'next/head';
+import CurrencyInfoHeroSection from '@/components/hero-sections/CurrencyInfoHeroSection';
 import CryptoInfoCard from '@/components/cards/CryptoInfoCard';
 import CryptoInfoList from '@/components/crypto-info-list/CryptoInfoList';
 import ArrowUp from '@/components/Ui/ArrowUp';
@@ -9,7 +10,6 @@ import { gsap } from 'gsap';
 export default function CurrencyInfoPage({ currencyData }) {
   const topFiveCurrencies = currencyData.slice(0, 5);
 
-  const heroSectionRef = useRef(null);
   const listRef = useRef(null);
   const cardsSectionRef = useRef(null);
 
@@ -42,29 +42,14 @@ export default function CurrencyInfoPage({ currencyData }) {
           content="Your accurate cryptocurrency data and information"
         ></meta>
       </Head>
-      <section
-        className="container mx-auto px-4 py-16 md:py-32"
-        ref={heroSectionRef}
-      >
-        <div className="md:w-2/4">
-          <h1 className="text-4xl">
-            Bitcoin and cryptocurrency for{' '}
-            <span className="text-purple-500">beginners</span>
-          </h1>
-          <h2 className="mt-4">
-            Get your information about the basics of cryprocurrencies from us.
-            We take a deep dive into different cryptocurrencies so that you can
-            be informed about them before you invest.
-          </h2>
-        </div>
-      </section>
+      <CurrencyInfoHeroSection />
       <section
         className="container mx-auto flex flex-col md:flex-row items-center flex-wrap gap-y-10 md:gap-16 md:justify-around py-10 lg:py-24"
         ref={cardsSectionRef}
       >
         {cryptoCards}
       </section>
-      <section className="container mx-auto " ref={listRef}>
+      <section className="container mx-auto" ref={listRef}>
         <h3 className="text-center text-4xl py-40">Currencies</h3>
         <CryptoInfoList currencyData={currencyData} />
         <div className="text-center py-20 lg:mt-36">
